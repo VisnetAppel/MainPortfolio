34c44716034123628b4a8c17126694de01376cb8
VisnetAppel <110181401+VisnetAppel@users.noreply.github.com>
2023-04-01 13:46:35 +0200

    made all the place holders. Only need to fill

--- resources/JS/test.js
+++ resources/JS/test.js
@@ -3,7 +3,7 @@
 
 document.getElementById("aboutMeButton").onclick = function(){
     document.getElementById("contentTitle").innerHTML = "About me";
-    document.getElementById("contentFill").innerHTML = "About me stuff";
+    document.getElementById("contentFill").innerHTML = "Try pressing the Past, Present and future button that have appeared to see what happens ;)";
     document.getElementById("certificates").style = "display: none;";
     document.getElementById("projects").style = "display: none;";
     document.getElementById("contactIconsRow").style = "display: none;";
@@ -14,7 +14,7 @@ document.getElementById("aboutMeButton").onclick = function(){
 
 document.getElementById("projectsButton").onclick = function() {
     document.getElementById("contentTitle").innerHTML = "Projects & Certificates";
-    document.getElementById("contentFill").innerHTML = "";
+    document.getElementById("contentFill").innerHTML = "Below you'll find the projects i've done so far and certificates i've collected";
     document.getElementById("certificates").style = "display: block;";
     document.getElementById("projects").style = "display: block;";
     document.getElementById("contactIconsRow").style = "display: none;";
@@ -38,14 +38,20 @@ document.getElementById("contactButton").onclick = function() {
 document.getElementById("pastButton").onclick = function(){
     document.getElementById("year").innerHTML = "2013";
     document.getElementById("contentFoto").innerHTML = '<img class="rounded" id="profileFoto" src="resources/Images/mijnGezichtVroeger.jpg">';
+    document.getElementById("contentTitle").innerHTML = "What i've done";
+    document.getElementById("contentFill").innerHTML = "What i've done";
 }
 
 document.getElementById("presentButton").onclick = function(){
     document.getElementById("year").innerHTML = new Date().getFullYear();
     document.getElementById("contentFoto").innerHTML = '<img class="rounded" id="profileFoto" src="resources/Images/mijnGezicht.jpg">';
+    document.getElementById("contentTitle").innerHTML = "What i'm doing now";
+    document.getElementById("contentFill").innerHTML = "What i'm doing now";
 }
 
 document.getElementById("futureButton").onclick = function(){
     document.getElementById("year").innerHTML = "2024";
     document.getElementById("contentFoto").innerHTML = '<img class="rounded" id="profileFoto" src="resources/Images/mijnGezichtToekomst.jpg">';
+    document.getElementById("contentTitle").innerHTML = "GOALS!";
+    document.getElementById("contentFill").innerHTML = '<ol><li>goal</li><li>goal</li><li>goal</li></ol>';
 }
